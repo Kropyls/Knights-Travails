4d6115f467f7d76b30bb3a00a6d8bf98faa6ccc8
Kropyls <Kropyls@gmail.com>
2022-08-23 22:07:27 -0700

    Built the meat of the program:
    Created knight_moves, find_destination_node and move_options methods.
    
    Added PositionAndOptions class to use as nodes in a tree.

--- knight.rb
+++ knight.rb
@@ -12,7 +12,20 @@ class Knight
     'K'
   end
 
-  def move_options(start = @position)
+  def knight_moves(destination, start = position)
+    dest_as_node = find_destination_node(destination, start)
+    path = []
+    until dest_as_node.nil?
+      path.unshift(dest_as_node.position)
+      dest_as_node = dest_as_node&.parent
+    end
+    puts "You've made it in #{path.count} moves! Path is:\n"
+    path.each { |coodinate| p coodinate }
+  end
+
+  private
+
+  def move_options(start = position)
     raise 'Error: starting off board dimensions.' unless start.all? { |coodinate| coodinate.between?(0, 7) }
 
     options = []
@@ -24,5 +37,27 @@ class Knight
     options
   end
 
-  def knight_moves(start, destination) end
+  def find_destination_node(destination, start)
+    queue = Queue.new
+    queue.push(PositionAndOptions.new(start))
+    until queue.empty?
+      node = queue.pop
+      return node if node.position == destination
+
+      node.children = move_options(node.position).map { |opt| PositionAndOptions.new(opt, node) }
+      node.children.each { |child| queue.push(child) }
+    end
+    raise "Fault: find_destination_node failed with: start: #{start} and destination: #{destination}."
+  end
+end
+
+# tracks current positon and options in a tree like state
+class PositionAndOptions
+  attr_accessor :position, :children, :parent
+
+  def initialize(position, parent = nil)
+    @position = position
+    @children = nil
+    @parent = parent
+  end
 end
